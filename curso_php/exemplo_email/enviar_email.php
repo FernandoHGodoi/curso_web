12f15aa31ccd9517f92a653463a9b549462420db
Fernando Godoi <90100824+FernandoHGodoi@users.noreply.github.com>
2022-02-05 11:19:01 -0300

    Add files via upload

---
+++ curso_php/exemplo_email/enviar_email.php
@@ -0,0 +1,9 @@
+<?php
+
+    $var_nome = $_POST['frm_nome'];
+    $var_email = $_POST['frm_email'];
+    $var_mensagem = $_POST['frm_mensagem'];
+
+    echo 'Nome: ' . $var_nome . '<br>Email: ' . $var_email . '<br>Mensagem: ' . $var_mensagem;
+
+?>
---
+++ curso_php/exemplo_email/formulario_email.php
@@ -0,0 +1,18 @@
+<form method="POST" action="enviar_email.php">
+
+    <input type="text" name="frm_nome" placeholder="Digite o nome">
+
+    <br>
+
+    <input type="text" name="frm_email" placeholder="Digite o email">
+
+    <br>
+
+    <input type="text" name="frm_mensagem" placeholder="Digite alguma mensagem">
+
+    <br>
+
+    <input type="submit" name="Enviar">
+
+</form>
+
